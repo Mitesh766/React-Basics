6f4395535b68a8c68bb361026dd397d143de093d
Mitesh Agrawal <miteshagrawal972@gmail.com>
2024-02-29 16:17:06 +0530

    Search Functionality

--- src/FoodDeliveryApp/Components/Body.js
+++ src/FoodDeliveryApp/Components/Body.js
@@ -3,37 +3,69 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 const Body = () => {
   //this is array destructuring
-
+  console.log("rendered");
   // same as let arr= useState(resList)
 
   //[ListofRes,setListOfRes]=arr
 
   // same as const ListofRes=arr[0]
   // same as const setListOfRes=arr[1]
+
   let [ListofRes, setListOfRes] = useState([]);
+  const [filteredListOfRes, setFilteredListOfRes] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-  fetchData()
+    fetchData();
   }, []);
 
-const fetchData=async ()=>{
-    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    const json=await data.json()
-    console.log(json)
+    const json = await data.json();
+    console.log(json);
 
-   // ? =>optional chaining
-    
-   setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-}
-   
-//while the data from api listOfRes will be empty  loads display this , 
-//conditional rendering
+    // ? =>optional chaining
+    setFilteredListOfRes(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setListOfRes(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //while the data from api listOfRes will be empty  loads display this ,
+  //conditional rendering
 
-// if length==0 return shimmer else return body-container
-  return ListofRes.length==0 ? (<Shimmer/> ) :(
+  // if length==0 return shimmer else return body-container
+  return ListofRes.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="searchArea"
+            // value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          {/* filtering data based on the search text */}
+          <button
+            onClick={() => {
+              SearchList = ListofRes.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredListOfRes(SearchList);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        {/* filtering data based on the average rating */}
         <button
           className="filter-btn"
           onClick={() => {
@@ -47,8 +79,9 @@ const fetchData=async ()=>{
           Top Rated Restaurants
         </button>
       </div>
+
       <div className="res-card-container">
-        {ListofRes.map((restaurant) => (
+        {filteredListOfRes.map((restaurant) => (
           <ResCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/FoodDeliveryApp/utils/constants.js
+++ src/FoodDeliveryApp/utils/constants.js
@@ -1,3 +1,3 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" 
 
-export const LOGO_URL="https://cdn5.vectorstock.com/i/1000x1000/46/84/glowing-neon-food-delivery-sign-with-delivering-vector-23574684.jpg"
+export const LOGO_URL="https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg"
